d7633a0eba364c5b039cf3f061b48e8b655ef9a6
Rui Freitas <dinnerpartysaboteur@gmail.com>
2019-05-21 10:28:10 +1000

    adds all Restaurant crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get    "restaurants",          to: "restaurants#index"
+
+  # post   "restaurants",          to: "restaurants#create"
+  # get    "restaurants/new",      to: "restaurants#new", as: :new_restaurant
+
+  # # NB: The `show` route needs to be *after* `new` route.
+  # get    "restaurants/:id",      to: "restaurants#show", as: :restaurant
+
+  # delete "restaurants/:id",      to: "restaurants#destroy"
+
+  # patch  "restaurants/:id",      to: "restaurants#update"
+  # get    "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+
+
+  # resources :restaurants, only: [:index, :show]
+  # resources :restaurants, except: [:destroy]
+
+  resources :restaurants
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_224308) do
+ActiveRecord::Schema.define(version: 2019_05_20_225616) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
 end
